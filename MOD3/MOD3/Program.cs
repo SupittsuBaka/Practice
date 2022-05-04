ba73d2902de134103f4b1193e84ba79091cae335
Admin-PC\Admin <sashashmidt02@gmail.com>
2022-05-04 18:31:32 +0300

    Add module 3

---
+++ MOD3/MOD3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MOD3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
